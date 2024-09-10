3f57ab443d71d1dd4c09dfe716df5397f826e220
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-09-10 21:07:06 +0530

    add components

--- src/Components/Game/game.jsx
+++ src/Components/Game/game.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './game.css'
 
+import circle from "../Assets/circle.jpg"
+import cross from "../Assets/cross.jpg"
+
 const game = () => {
   return (
-    <div>game</div>
+    <div className='container'>
+      <h1 className='title'> Game with <span>React</span> </h1>
+    <div className="board">
+
+    </div>
+    <button className="reset">Reset</button>
+    </div>
   )
 }
 
