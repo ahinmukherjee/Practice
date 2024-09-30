316ed1227d002806f193f55d80196e707f913dbd
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-09-30 20:56:39 +0530

    added function

--- src/Components/Page/game.jsx
+++ src/Components/Page/game.jsx
@@ -10,7 +10,9 @@ const Game = () => {
 
   let [count,setCount] = useState(0);
   let [lock,setLock] = useState(false);
-  // let titleRef =useRef(null);
+  let titleRef =useRef(null);
+
+
   let box1 =useRef(null);
   let box2 =useRef(null);
   let box3 =useRef(null);
@@ -36,6 +38,60 @@ const Game = () => {
       data[num] = "o";
       setCount(++count);
     }
+    checkWin();
+  }
+
+
+  const checkWin = () =>{
+    if(data[0]==data[1] && data[1]==data[2] && data[2]!=="")
+      {
+      won(data[2]);
+      }
+    else if(data[3]==data[4] && data[4]==data[5] && data[5]!=="")
+      {
+      won(data);
+      }
+    else if(data[6]==data[7] && data[7]==data[8] && data[8]!=="")
+      {
+      won(data);
+      }
+    else if(data[0]==data[3] && data[3]==data[6] && data[6]!=="")
+      {
+        won(data);
+      }
+    else if(data[1]==data[4] && data[4]==data[7] && data[7]!=="")
+      {
+        won(data);
+      }
+    else if(data[2]==data[5] && data[5]==data[8] && data[8]!=="")
+      {
+        won(data);
+      }
+    else if(data[0]==data[4] && data[4]==data[8] && data[8]!=="")
+      {
+        won(data);
+      }
+    else if(data[0]==data[1] && data[1]==data[2] && data[2]!=="")
+      {
+        won(data);
+      }
+    else if(data[2]==data[4] && data[4]==data[6] && data[6]!=="")
+      {
+        won(data);
+      }
+        
+  }
+
+  const won = (winnner) =>{
+    setLock(true);
+    if(winnner==="x")
+    {
+      titleRef.current.innerHTML = `Congratulations: <img src=${cross}>`
+    }
+    else
+    {
+      titleRef.current.innerHTML = `Congratulations: <img src=${circle}}>`
+    }
   }
 
   const reset = () =>{
@@ -49,7 +105,7 @@ const Game = () => {
 
   return (
     <div className='container'>
-      <h1 className='title'> Game with <span>React</span> </h1>
+      <h1 className='title'ref={titleRef}> Game with <span>React</span> </h1>
     <div className="board">
       <div className="row1">
         <div className="boxes" ref={box1} onClick={(e)=> {toggle(e,0)}}></div>
